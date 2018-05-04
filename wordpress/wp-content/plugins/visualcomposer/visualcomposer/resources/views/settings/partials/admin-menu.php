ec648bb1cf49f69668352a874dc0a3050a122810
jamer-sysops <sysops@adbusters.org>
2018-05-04 12:04:06 -0700

    fixed margins

--- wordpress/wp-content/plugins/visualcomposer/visualcomposer/resources/views/settings/partials/admin-menu.php
+++
@@ -1,6 +0,0 @@
-<style>
-    /** Fixes min-width for long sidebar nav **/
-    #adminmenu .wp-not-current-submenu .wp-submenu li.vcv-dashboard-admin-menu--add {
-        min-width: 220px;
-    }
-</style>
